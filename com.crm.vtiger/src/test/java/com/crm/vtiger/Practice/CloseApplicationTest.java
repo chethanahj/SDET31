36a03e86b7cc6831fc46bcaa488dfc7c9f983c5a
admin <admin@DESKTOP-58TVSAQ>
2022-03-23 17:08:57 +0530

    first one

---
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Practice/CloseApplicationTest.java
@@ -0,0 +1,16 @@
+package com.crm.vtiger.Practice;
+
+import org.testng.annotations.Test;
+
+public class CloseApplicationTest {
+	@Test
+	public void closeApplication() {
+		System.out.println("Application is closed");
+	}
+	
+    @Test
+	public void deleteApplication() {
+		System.out.println("Application is deleted");
+	}
+
+}
