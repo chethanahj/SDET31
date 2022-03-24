77c1494c065c26bfc66c5e548603f670e8130b1d
admin <admin@DESKTOP-58TVSAQ>
2022-03-24 14:23:41 +0530

    push method 2

--- com.crm.vtiger/src/test/java/com/crm/vtiger/Practice/CreateCampaignTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Practice/CreateCampaignTest.java
@@ -12,4 +12,9 @@ public class CreateCampaignTest {
 	public void createCampaign1() {
 		System.out.println("campaign 1 created");
 	}
+	@Test
+	public void createCampaign2() {
+		System.out.println("campaign  2 created");
+	
+}
 }
