fb0dc6a9cf5631e1992959a0a52e2e9d10a83532
admin <admin@DESKTOP-58TVSAQ>
2022-03-24 21:46:46 +0530

    commit45

--- com.crm.vtiger/src/test/java/com/crm/vtiger/Practice/CloseApplicationTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Practice/CloseApplicationTest.java
@@ -8,6 +8,10 @@ public class CloseApplicationTest {
 		System.out.println("Application is closed");
 	}
 	
+	@Test
+	public void closeApplication2() {
+		System.out.println("Application 2 is closed");
+	}
     @Test
 	public void deleteApplication() {
 		System.out.println("Application is deleted");
