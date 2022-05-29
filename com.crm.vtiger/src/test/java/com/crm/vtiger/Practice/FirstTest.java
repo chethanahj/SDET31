5add94229f92d17f1f67a7be67e6e13504210ff0
chethanahj <chethana0016@gmail.com>
2022-05-29 10:36:02 +0530

    branch

--- com.crm.vtiger/src/test/java/com/crm/vtiger/Practice/FirstTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/Practice/FirstTest.java
@@ -4,7 +4,9 @@ public class FirstTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("abc");
+System.out.println("abc"
+		+ ""
+		+ "");
 	}
 
 }
